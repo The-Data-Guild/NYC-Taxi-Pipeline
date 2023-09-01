865bccd9a38ffe1775b27969772b9100d6497653
AliShaheed <alishaheed1138@gmail.com>
2023-09-01 19:28:56 +0100

    Added DB config settings

--- src/nyc_taxi/conf_manager.py
+++ src/nyc_taxi/conf_manager.py
@@ -11,5 +11,12 @@ DOWNLOADS_PATH = os.path.join(PARENT_DIR, 'raw')
 CLEAN_DATA_PATH = os.path.join(PARENT_DIR, 'clean')
 CSV_PATH = os.path.join(DOWNLOADS_PATH, 'data.csv')
 
+DB_PATH = os.path.join(PARENT_DIR, 'data')
+DATABASE = conf("DATABASE")
+DB_USER = conf("DB_USER")
+DB_PASSWORD = conf("DB_PASSWORD")
+HOST = conf("HOST")
+PORT = conf("PORT")
+
 if __name__ == "__main__":
     print(DATA_URL) # should print the url of where the data is being downloaded from
