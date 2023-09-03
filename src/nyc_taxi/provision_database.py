adb7f8c27fdd0eeeb48f95027f0ae1d42d4291aa
AliShaheed <alishaheed1138@gmail.com>
2023-09-03 18:29:33 +0100

    Update provision_database.py

--- src/nyc_taxi/provision_database.py
+++ src/nyc_taxi/provision_database.py
@@ -1,7 +1,6 @@
 import psycopg2
-from psycopg2 import sql
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
-from conf_manager import DB_USER, DB_PASSWORD, HOST, PORT, DATABASE, DB_PATH, TABLESPACE, MAIN_DB
+from conf_manager import DB_USER, DB_PASSWORD, HOST, PORT, DATABASE, MAIN_DB #pylint: disable=import-error
 
 conn = psycopg2.connect(database=MAIN_DB,
                         user=DB_USER,
@@ -13,8 +12,5 @@ conn = psycopg2.connect(database=MAIN_DB,
 conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
 
 with conn.cursor() as curs:
-    curs.execute(sql.SQL("CREATE TABLESPACE {tablespace} LOCATION {db_path};")
-                 .format(tablespace = sql.Identifier(TABLESPACE),
-                         db_path = sql.Literal(DB_PATH)))
     curs.execute(f"CREATE DATABASE {DATABASE};")
-    print(f"Database {DATABASE} SET TABLESPACE '{DB_PATH}'")
+    print(f"Database {DATABASE} created.")
