7aa960708f8019bf105c2d16b94a3be29ee09561
AliShaheed <alishaheed1138@gmail.com>
2023-09-04 18:03:41 +0100

    Update provision_database.py

--- src/nyc_taxi/provision_database.py
+++ src/nyc_taxi/provision_database.py
@@ -9,16 +9,25 @@ from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 from conf_manager import DB_USER, DB_PASSWORD, HOST, PORT, DATABASE, MAIN_DB #pylint: disable=import-error
 
 # create db connection
-conn = psycopg2.connect(database=MAIN_DB,
+def db_connect(db):
+    """function to create db connection"""
+    connection = psycopg2.connect(database=db,
                         user=DB_USER,
                         password=DB_PASSWORD,
                         host=HOST,
                         port=PORT)
-
-
-conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+    connection.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+    return connection
 
 # create database
-with conn.cursor() as curs:
-    curs.execute(f"CREATE DATABASE {DATABASE};")
-    print(f"Database {DATABASE} created.")
+def create_db(connection):
+    """function to create db"""
+    with connection.cursor() as curs:
+        curs.execute(f"CREATE DATABASE {DATABASE};")
+        print(f"Database {DATABASE} created.")
+    return True
+
+if __name__ == '__main__':
+    con = db_connect(MAIN_DB)
+    # create database
+    create_db(con)
